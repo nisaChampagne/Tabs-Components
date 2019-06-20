0c3e8886814cd1bc1e28378738d04530b5f93965
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-20 18:30:19 -0400

    making some sense

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -68,9 +68,15 @@
 
 // - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
 
-// - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
+// // - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
 
 // */
+
+
+/////THIS IS WHERE IT STARTS
+
+
+
 
 class TabLink {
   constructor(element) {
@@ -78,23 +84,23 @@ class TabLink {
 
     this.element = element;
 
-    // console.log(this.element);
+    // console.log(this.element);// SHOWS MY FULL TABS-LINK FROM DIV TO DIV
     
     // Get the custom data attribute on the Link
 
     this.data = this.element.dataset.tab;
-    // console.log(this.data); ///shows 1,2,3,4
+    // console.log(this.data); ///SHOWS THE NUMBER WITHIN THE DATA-TAB ON EACH TAB I.E 12345678
 
 
     this.item = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-    // console.log(this.item);/// shows  tabs 1-4 and tab item description 1-4
+    // console.log(this.item);///  SHOWS DATA-TAB, INNER TAB TITLE AND DESCRIPTION
+
 
     // Using the custom data attribute get the associated Item element
-    this.itemElement = new TabItem(this.item);
-    // console.log(this.itemElement);
     
-    // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.itemElement = new TabItem(this.item);
+    // console.log(this.itemElement);//// SHOWS THE NEW TABITEM AS AN ELEMENT/OBJECT TO WHERE WE CAN ADD THE ADDEVENTLISTENER
+  
     
     // Add a click event listener on this instance, calling the select method on click
 
@@ -102,15 +108,23 @@ class TabLink {
 
   };
 
-  select() {
+  select() {////an action within TabLink
+
     // Get all of the elements with the tabs-link class
+    
     const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+   
     links.forEach(link => link.classList.remove('tabs-link-selected'));
+////when you click from 1 -2 it will honor the red color and show as if it wasn't selected. If you comment this out it will show everything as selected forever.
+
 
     // Add a class named "tabs-link-selected" to this link
+
     this.element.classList.add('tabs-link-selected');
+////when you comment this out it doesnt show the selected item when you click on it. keeping it in the code will show which tab you select.
+
     
     // Call the select method on the item associated with this link
     this.itemElement.select();
@@ -123,7 +137,7 @@ class TabItem {
     this.element = element;
   }
 
-  select() {
+  select() {///an action within TabItem when clicking the tabs
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
@@ -135,7 +149,7 @@ class TabItem {
   }
 }
 
-
+/////THIS SELECTS MY TABS
 const links = document.querySelectorAll('.tabs-link');
 links.forEach(link => new TabLink(link));
-console.log(links);
+// console.log(links);//// shows my tablink/buttons
